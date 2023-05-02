78251e344b845ab4276d694321dfa6680f078999
Konstantin0 <maschkovc@yandex.ru>
2023-05-02 11:19:44 +0300

    2.5

---
+++ 2.5Шаблонные строки/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Шаблонные строки
+// важность: 5
+// Что выведет этот скрипт?
+
+let name1 = "Ilya";
+
+alert( `hello ${1}` ); // hello 1 ?
+
+alert( `hello ${"name"}` ); // hello name ?
+
+alert( `hello ${name1}` ); // hello Ilya ?
