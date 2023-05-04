53accc597882d7256ecd3e9610a7b534ba1f5605
Konstantin0 <maschkovc@yandex.ru>
2023-05-04 09:50:27 +0300

    2.9

---
+++ 2.9Операторы сравнения/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Операторы сравнения
+// важность: 5
+// Каким будет результат этих выражений?
+
+// 5 > 4 = true
+// "ананас" > "яблоко" = t
+// "2" > "12" = t
+// undefined == null = t
+// undefined === null = f
+// null == "\n0\n" = f
+// null === +"\n0\n" = f
+
+console.log(undefined == null); 
