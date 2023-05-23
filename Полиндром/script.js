19b0b283d87d67ff2ea7a3a674ac2bacce2016d1
Konstantin0 <maschkovc@yandex.ru>
2023-05-23 09:30:24 +0300

    Полиндром вариант 2

--- Полиндром/script.js
+++ Полиндром/script.js
@@ -1,10 +1,22 @@
 'use strict';
+// 1
+
+let str = "zzz1";
 
 function polindrom(str) {
     str.toLowerCase();
 
     return str === str.split("").reverse().join("");
 }
+console.log(polindrom(str));
 
+// 2
 
-console.log(polindrom("фхфф"));
+function polindrom2(str) {
+    let res = "";
+    for(let i = str.length - 1; i >= 0; i--) {
+        res += str[i];
+    }  
+    return str === res;
+}
+console.log(polindrom(str));
